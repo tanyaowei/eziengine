33327c1ec23c8a4224f81584c89524d204b36627
blazex47 <blazex.net@hotmail.com>
2019-10-28 22:20:37 +0800

    change env variable name

--- buildweb.sh
+++ buildweb.sh
@@ -4,11 +4,11 @@ BASE_PATH=`dirname $SCRIPT_PATH`
 cd $BASE_PATH
 
 source /var/common/emsdk/emsdk_env.sh
-EMSCRIPTEN_ROOT_PATH=$EMSDK/upstream/emscripten
+EMSCRIPTEN=$EMSDK/upstream/emscripten
 mkdir -p build
 mkdir -p install
 cd build
-time cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_ROOT_PATH/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_INSTALL_PREFIX=$BASE_PATH/install ..
+time cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_INSTALL_PREFIX=$BASE_PATH/install ..
 time make -j12
 time make install
 
